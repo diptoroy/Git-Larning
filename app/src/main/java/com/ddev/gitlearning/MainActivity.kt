fa05415d088f2fdfda317353c74e7e847c404daf
diptoroy <diptoroy54@gmail.com>
2022-02-08 16:00:35 +0600

    add random

--- app/src/main/java/com/ddev/gitlearning/MainActivity.kt
+++ app/src/main/java/com/ddev/gitlearning/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.ddev.gitlearning
 
+import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.ddev.gitlearning.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        var randomNumber = (0..20).random()
+
+        binding.randomBtn.setOnClickListener {
+            binding.digitTextView.text = randomNumber.toString()
+        }
+
     }
 }
