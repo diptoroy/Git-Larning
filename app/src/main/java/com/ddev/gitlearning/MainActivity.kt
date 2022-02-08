3dd719d78a94acd4b05bbf6c0d8cebdd2c98c6d1
diptoroy <diptoroy54@gmail.com>
2022-02-08 15:37:33 +0600

    add repo

---
+++ app/src/main/java/com/ddev/gitlearning/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ddev.gitlearning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
