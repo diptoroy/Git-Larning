356ac7671047d0b52258e5089b39a60306db0ee4
diptoroy <diptoroy54@gmail.com>
2022-02-08 18:36:41 +0600

    counter problem solved

--- app/src/main/java/com/ddev/gitlearning/MainActivity.kt
+++ app/src/main/java/com/ddev/gitlearning/MainActivity.kt
@@ -9,16 +9,28 @@ import com.ddev.gitlearning.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    var count:Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.randomBtn.setOnClickListener {
-            var randomNumber = (0..20).random()
-            binding.digitTextView.text = randomNumber.toString()
+//        binding.randomBtn.setOnClickListener {
+//            var randomNumber = (0..20).random()
+//            binding.digitTextView.text = randomNumber.toString()
+//        }
+
+        binding.increaseBtn.setOnClickListener {
+            count++
+            binding.digitTextView.text = count.toString()
         }
 
+        binding.decreaseBtn.setOnClickListener {
+            if(count != 0) {
+                count--
+                binding.digitTextView.text = count.toString()
+            }
+        }
     }
 }
